ad7d3d4c0bbb01116e96312362c5a2d6e9a71c96
sweetybagde24 <103164782+sweetybagde24@users.noreply.github.com>
2022-07-07 07:32:35 +0530

    Add files via upload

---
+++ pr-4-3.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+int main() {
+const int x = 10;
+const float y = 4.14;
+const char z = 'A';
+clrscr();
+cout << "Int Constant: " <<x<<endl;
+cout << "Float Constant: " <<y<<endl;
+cout << "Char Constant: "<<z<<endl;
+return 0;
+}
