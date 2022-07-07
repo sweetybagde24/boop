68c44be30d02a947fe3a61da3fe8d5258ea47796
sweetybagde24 <103164782+sweetybagde24@users.noreply.github.com>
2022-07-07 07:26:02 +0530

    Add files via upload

---
+++ pr-4-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+void swap (int &num1, int &num2)
+{
+	int temp;
+	temp=num1;
+	num1=num2;
+	num2=temp;
+}
+int main()
+{int a=5,b=10;
+cout<<"\n Before swapping"<<"\n A = "<<a<<"\n B = "<<b<<endl;
+swap(a,b);
+cout<<"\n After swapping"<<"\n A = "<<a<<"\n B = "<<b<<endl;
+getch();
+return 0;
+}
